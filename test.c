4b788600cff2461ef836e176ebbef012d5cb980f
Taiga Okuma <toa12@sfu.ca>
2023-10-31 17:52:59 -0700

    Test Commit by Tiger

--- test.c
+++ test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int result = 0;
+
+int add(int a, int b){
+  result =  a + b;
+  return result;
+};
+
+
 int main() {
-  printf("Hello World");
+  add(20,2039102109);
+  printf("Output: %d\n", result);
   return 0;
 }
 
