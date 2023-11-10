f86a8a3f3c4c1ce4155621db2feb298b2ce1e358
Anna Rusinova <ara103@sfu.ca>
2023-11-10 07:40:37 -0800

    feat: add an image preview functionality

--- colour-palette/src/App.js
+++ colour-palette/src/App.js
@@ -3,17 +3,20 @@ import ColorThief from 'colorthief';
 import './App.css';
 
 function App() {
-  const [image, setImage] = useState(null);
-  const [colors, setColors] = useState([]);
-  const [numberOfColors, setNumberOfColors] = useState(5);
+  const [image, setImage] = useState(null); // Holds the image URL
+  const [colors, setColors] = useState([]); // Stores an array of the extracted colors
+  const [numberOfColors, setNumberOfColors] = useState(5);  // Number of colors to extract (5 by default)
+  const [isImagePreviewActive, setIsImagePreviewActive] = useState(true);
   const imgRef = useRef(null); // Create a reference to the img tag
-  const colorThief = new ColorThief();
+  const colorThief = new ColorThief(); 
+  // const [loading, setLoading] = useState(false); // Loading state
 
-  const [darkTheme, setDarkTheme] = useState(true);
+  const [darkTheme, setDarkTheme] = useState(true); // Stores the current theme
   const toggleTheme = () => {
     setDarkTheme(!darkTheme);
   } ;
 
+  // Handle the file upload
   const handleImageChange = (event) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
@@ -21,40 +24,68 @@ function App() {
 
       reader.onload = (e) => {
         setImage(e.target.result); // Set image URL to display it
-        // Once the image is loaded, the onLoad event on the img element will trigger color extraction
+        setIsImagePreviewActive(false); // Set image preview active
       };
-
       reader.readAsDataURL(file);
     }
   };
+
+  // Handle closing the image preview
+  const handleClosePreview = () => {
+    setImage(null); // Reset the image state to close the preview
+    setColors([]); // Clear the colors when closing the preview
+    setIsImagePreviewActive(true); // Set image preview inactive
+  };
+
+  // Convert RGB values to HEX format
   const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
     const hex = x.toString(16);
     return hex.length === 1 ? '0' + hex : hex;
   }).join('');
   
+
+  // Extract colors from the loaded image using ColorThief, and update the state
   const extractColors = () => {
     if (imgRef.current && imgRef.current.complete) {
       // Ensure the image is loaded and complete
       try {
-        const result = colorThief.getPalette(imgRef.current, numberOfColors);
+        // It has the following syntax: getPalette(image [, colorCount, quality])
+        const result = colorThief.getPalette(imgRef.current, numberOfColors, 10);
         // Convert the RGB values to HEX and update the state
         setColors(result.map(rgb => rgbToHex(...rgb)));
       } catch (error) {
         console.error('Error extracting the colors:', error);
       }
+    } else {
+      imgRef.current.addEventListener('load', function() {
+        // Ensure the image is loaded and complete
+        try {
+          const result = colorThief.getPalette(imgRef.current, numberOfColors, 10);
+          // Convert the RGB values to HEX and update the state
+          setColors(result.map(rgb => rgbToHex(...rgb)));
+        } catch (error) {
+          console.error('Error extracting the colors:', error);
+        }
+      });
     }
   };
-
+  
+  // Handle the number of colors to extract
   const handleNumberChange = (event) => {
     setNumberOfColors(event.target.value);
-    // Trigger re-extraction of colors if the image is already loaded
+    // That's for handling the case when slider is moved before the image is loaded
     if (imgRef.current && imgRef.current.complete) {
+      // If the image is already loaded, extract colors
       extractColors();
+    } else {
+      // If the image is not loaded, wait for it to load and then extract colors
+      imgRef.current.addEventListener('load', function() {
+        extractColors();
+      });
     }
   };
 
- 
-
+  // Render the app
   return (
     <div className="App" data-theme={darkTheme ? 'dark' : 'light'}>
       {/* Theme switcher button */}
@@ -64,54 +95,81 @@ function App() {
         </button>
       </div>
 
-      <header className="App-header">
+      <main className="app-content">
+        {/* Left column */}
+        <section className="content-block">
+
+        <header className="text_block">
         <h1>Color Picker</h1>
         <p>Extract wonderful palettes from your image.</p>
-      </header>
-      <main className="App-content">
-        <section className="upload-area">
-          <input type="file" accept="image/*" onChange={handleImageChange} id="fileInput" />
-          <label htmlFor="fileInput">
-            <div className="upload-icon">+</div>
-            <p>Click or drag file to this area to upload</p>
-            <div className="file-size-info">
-              <i className="info-icon">i</i> Max file size: XX MB
-            </div>
-          </label>
+        </header>
+
+            {isImagePreviewActive && (
+            <section className="upload-area">
+              <input type="file" accept="image/*" onChange={handleImageChange} id="fileInput" />
+              <label htmlFor="fileInput">
+              <header className="text_block">
+                <div className="text_block_text">
+                  <img 
+                    src={"/images/upload-icon.svg"}
+                    alt="Upload icon"
+                    style={{ 
+                        width: '30px', 
+                        height: '30px',
+                        strokeWidth: '1.5px',
+                        stroke: 'var(--dark-050, rgba(18, 18, 18, 0.50))'
+                   }}/>
+                  <p>Click or drag file to this area to upload</p>
+                </div>
+                <div className="text_block_subtext">
+                  <i className="info-icon">i</i> Max file size: XX MB
+                </div>
+              </header>
+              </label>
+            </section>
+            )}
+            
+            {image && (
+              <div className="image-preview">
+                <button className="close-button" onClick={handleClosePreview}>
+                  <span>&times;</span>
+                </button>
+
+                <img
+                  ref={imgRef}
+                  src={image}
+                  alt="To extract colors from"
+                  crossOrigin="anonymous"
+                  onLoad={extractColors}
+                />
+              </div>
+            )}
+      
+            <section className="color-controls">
+              <input
+                type="range"
+                min="2" 
+                max="10"
+                value={numberOfColors}
+                onChange={handleNumberChange}
+              />
+              <p>Number of colors: {numberOfColors}</p>
+            </section>
+          {/* </div> */}
         </section>
-
-        {image && (
-          <img
-            ref={imgRef}
-            src={image}
-            alt="To extract colors from"
-            crossOrigin="anonymous"
-            onLoad={extractColors}
-            style={{ display: 'none' }}
-          />
-        )}
-
-        <section className="color-controls">
-          <input
-            type="range"
-            min="1"
-            max="10"
-            value={numberOfColors}
-            onChange={handleNumberChange}
-          />
-          <p>Number of colors: {numberOfColors}</p>
-        </section>
-
-        <aside className="color-palette">
+        
+        {/* Right column */}
+        <section className="color-palette">
           {colors.map((color, index) => (
-            <div key={index} className="color" style={{ backgroundColor: color }}>
-              <p>{color}</p>
-            </div>
+          <div key={index} className="color" style={{ backgroundColor: color }}>
+            <p>{color}</p>
+          </div>
           ))}
-        </aside>
+        </section>
       </main>
     </div>
   );
 }
 
 export default App;
+
