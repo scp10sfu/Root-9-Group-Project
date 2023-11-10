38af6e793f7b06e7bf2d185137f7f8c5fe076dc6
Taiga Okuma <toa12@sfu.ca>
2023-11-09 19:34:50 -0800

    Installed color-theif API by Taiga Okuma

--- colour-palette/src/App.js
+++ colour-palette/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [colors, setColors] = useState([]);
+  const [numberOfColors, setNumberOfColors] = useState(5);
+
+  const handleImageChange = (event) => {
+    // Logic to handle image file
+    // And extract colors from the image
+  };
+
+  const handleNumberChange = (event) => {
+    setNumberOfColors(event.target.value);
+    // Logic to update the number of colors extracted
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Color Picker</h1>
+        <p>Extract wonderful palettes from your image.</p>
       </header>
+      <main>
+        <section className="upload-area">
+          <input type="file" onChange={handleImageChange} />
+          <p>Click or drag file to this area to upload</p>
+        </section>
+        <section className="color-controls">
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={numberOfColors}
+            onChange={handleNumberChange}
+          />
+          <p>Number of colors: {numberOfColors}</p>
+        </section>
+        <aside className="color-palette">
+          {colors.map((color, index) => (
+            <div key={index} className="color" style={{ backgroundColor: color.hex }}>
+              <p>{color.hex}</p>
+              {/* Other color information */}
+            </div>
+          ))}
+        </aside>
+      </main>
     </div>
   );
 }
