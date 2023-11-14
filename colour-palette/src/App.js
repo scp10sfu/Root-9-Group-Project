5b0a80800faa820f13c81ceffca06ca7aaaaccb2
Taiga Okuma <toa12@sfu.ca>
2023-11-14 10:09:17 -0800

    Added RGB,CMYK color number

--- colour-palette/src/App.js
+++ colour-palette/src/App.js
@@ -48,16 +48,18 @@ function App() {
         const result = colorThief.getPalette(imgRef.current, numberOfColors, 10);
         const colorPromises = result.map(async (rgb) => {
           const hex = rgbToHex(...rgb);
+          const cmyk = rgbToCmyk(...rgb);
           const name = await fetchColorName(hex);
-          return { hex, name }; // Return an object with hex and name
+          return { hex, rgb: `(${rgb.join(', ')})`, cmyk: `(${cmyk.join(', ')})`, name };
         });
-        const colorObjects = await Promise.all(colorPromises); // Resolve all promises
-        setColors(colorObjects); // Set the colors state with an array of color objects
+        const colorObjects = await Promise.all(colorPromises);
+        setColors(colorObjects);
       } catch (error) {
         console.error('Error extracting the colors:', error);
       }
     }
   };
+  
 
   // Function to fetch color name from the API
   const fetchColorName = async (hex) => {
@@ -92,7 +94,25 @@ function App() {
   const toggleTheme = () => {
     setDarkTheme(!darkTheme); // This will switch the state between true and false
   };
-
+   
+  const rgbToCmyk = (r, g, b) => {
+    let c = 1 - (r / 255);
+    let m = 1 - (g / 255);
+    let y = 1 - (b / 255);
+    let k = Math.min(c, Math.min(m, y));
+  
+    c = ((c - k) / (1 - k)) || 0;
+    m = ((m - k) / (1 - k)) || 0;
+    y = ((y - k) / (1 - k)) || 0;
+  
+    c = Math.round(c * 100);
+    m = Math.round(m * 100);
+    y = Math.round(y * 100);
+    k = Math.round(k * 100);
+  
+    return [c, m, y, k];
+  };
+  
   // Render the app
   return (
     <div className="App" data-theme={darkTheme ? 'dark' : 'light'}>
@@ -157,14 +177,19 @@ function App() {
         </section>
 
         {/* Right column */}
-        <section className="color-palette">
-          {colors.map((colorObj, index) => (
-            <div key={index} className="color" style={{ backgroundColor: colorObj.hex }}>
-              <p className="color-name">{colorObj.name}</p> {/* Color name is displayed first */}
-              <p className="color-hex">{colorObj.hex}</p> {/* Then the HEX code */}
-            </div>
-          ))}
-        </section>
+       {/* Right column */}
+       <section className={`color-palette ${colors.length === 2 ? 'two-colors' : colors.length === 8 ? 'eight-colors' : ''}`}>
+       {colors.map((colorObj, index) => (
+  <div key={index} className="color" style={{ backgroundColor: colorObj.hex }}>
+    <p className="color-name">{colorObj.name}</p>
+    <p className="color-hex">HEX: {colorObj.hex}</p>
+    <p className="color-rgb">RGB: {colorObj.rgb}</p>
+    <p className="color-cmyk">CMYK: {colorObj.cmyk}</p>
+  </div>
+))}
+
+</section>
+
       </main>
     </div>
   );
