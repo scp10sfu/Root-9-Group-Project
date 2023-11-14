d01b317056003280f3216a256bb3f3772df673e6
Anna Rusinova <ara103@sfu.ca>
2023-11-14 00:24:31 -0800

    feat: add new folders and files to the project structure

---
+++ colour-palette/src/Pages/MoodboardGenerator.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>
+                This is a colour extractor.
+            </h1>
+        </div>
+    );
+
+};
+
+export default About;
