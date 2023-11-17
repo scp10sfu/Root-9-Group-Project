40ba73514a1c4df85c2588f2aa1a7e133b57fb70
Anna Rusinova <ara103@sfu.ca>
2023-11-17 01:18:20 -0800

    feat: add a 32-column grid layout

--- colour-palette/src/Pages/Home.js
+++ colour-palette/src/Pages/Home.js
@@ -7,6 +7,7 @@ import Layout from '../Components/Layout';
 const Home = () => {
 
   return (
+    <body>
     <Layout>
 
       {/* <main class="container">
@@ -40,98 +41,83 @@ const Home = () => {
   </section>
 </main> */}
 
-  {/* <NavigationBar /> */}
-
+      {/* <NavigationBar /> */}
       <div class="grid-container general-demo">
         {/* The nav bar */}
-        <div class="logo col-xs-2 col-md-2"></div>
-        <div class="header col-xs-10 col-md-10"></div>
+        <div class="logo col-xs-6 col-md-6"></div>
+        <div class="header col-xs-30 col-md-30"></div>
 
         {/* The main content - left part */}
-        <div class="main-section col-xs-12 col-md-5 grid-container nested-grid">
-          <div class="col-xs-12 col-md-12 grid-container secondary-nested-grid">
-            <div class="col-xs-10 col-md-10 grid-container col-xs-offset-2 secondary-nested-grid">
+        <div class="main-section col-xs-36 col-md-12 grid-container nested-grid">
 
-              <div class="col-xs-12">TITLE</div>   {/* Title */}
-              <div class="col-xs-12">Subtitle</div>   {/* Extra information */}
-              <div class="col-xs-12">Upload image:</div>  {/* Upload image */}
-              <div class="col-xs-12">IMAGE:</div>  {/* Upload image */}
-              <div class="col-xs-12">Setting number</div>  {/* Setting number */}
-            </div> 
-           
-          </div>
+          {/* <div class="col-xs-36 col-md-24 grid-container secondary-nested-grid"> */}
+          {/* <div class="col-xs-36 col-md-25 secondary-nested-grid"> */}
+
+          <div class="col-xs-36 col-md-25">TITLE</div>   {/* Title */}
+          <div class="col-xs-36 col-md-25">Subtitle</div>   {/* Extra information */}
+          <div class="col-xs-36 col-md-25">Upload image:</div>  {/* Upload image */}
+          <div class="col-xs-36 col-md-25">IMAGE:</div>  {/* Upload image */}
+          <div class="col-xs-36 col-md-25">Setting number</div>  {/* Setting number */}
+
+          {/* </div>  */}
+          {/* </div> */}
+
         </div>
 
         {/* The main content - right part: DOMINANT COLOURS */}
-        <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
+        <div class="secondary-section col-xs-36 col-md-24 grid-container nested-grid">
           {/* First dominant colour */}
-          <div class="col-xs-12 col-md-6"></div>
+          <div class="col-xs-36 col-md-18">Dominant colour</div>
           {/* Second dominant colour */}
-          <div class="col-xs-12 col-md-6"></div>
+          <div class="col-xs-36 col-md-18">Dominant colour</div>
         </div>
 
-     
-        {/* <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
-          <div class="col-xs-12 col-md-12"></div>
-        </div>
 
-        <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
-          <div class="col-xs-12 col-md-6"></div>
-          <div class="col-xs-12 col-md-6"></div>
-        </div> */}
-
-        {/* 5 colours 
+        {/* 4 colours 
           * TODO: add conditon
           */}
-        {/* <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
-          <div class="col-xs-12 col-md-4"></div>
-          <div class="col-xs-12 col-md-4"></div>
-          <div class="col-xs-12 col-md-4"></div>
+        {/* <div class="secondary-section col-xs-36 col-md-24 grid-container nested-grid">
+          <div class="col-xs-36 col-md-18">Colour #3</div>
+          <div class="col-xs-36 col-md-18">Colour #4</div>
         </div> */}
 
-{/* 
-        <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
-          <div class="col-xs-12 col-md-3"></div>
-          <div class="col-xs-12 col-md-3"></div>
-          <div class="col-xs-12 col-md-3"></div>
-          <div class="col-xs-12 col-md-3"></div>
-      </div> */}
 
-        <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-        </div>
-
-        <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-          <div class="col-xs-12 col-md-1"></div>
-        </div>
-
-
-        {/*
-        <div class="secondary-section col-xs-12 col-md-7 grid-container nested-grid">
-           <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
-          <div class="col-xs-12 col-md-2"></div>
+        {/* 6 colours 
+          * TODO: add conditon
+          */}
+        {/* <div class="secondary-section col-xs-36 col-md-24 grid-container nested-grid">
+          <div class="col-xs-36 col-md-9">Colour #3</div>
+          <div class="col-xs-36 col-md-9">Colour #4</div>
+          <div class="col-xs-36 col-md-9">Colour #5</div>
+          <div class="col-xs-36 col-md-9">Colour #6</div>
         </div> */}
+
+        {/* 8 colours */}
+        <div class="secondary-section col-xs-36 col-md-24 grid-container nested-grid">
+          <div class="col-xs-36 col-md-6">Colour #3</div>
+          <div class="col-xs-36 col-md-6">Colour #4</div>
+          <div class="col-xs-36 col-md-6">Colour #5</div>
+          <div class="col-xs-36 col-md-6">Colour #6</div>
+          <div class="col-xs-36 col-md-6">Colour #7</div>
+          <div class="col-xs-36 col-md-6">Colour #8</div>
+        </div>
+
+        {/* 10 colours */}
+        <div class="secondary-section col-xs-36 col-md-24 grid-container nested-grid">
+          <div class="col-xs-36 col-md-9">Colour #3</div>
+          <div class="col-xs-36 col-md-9">Colour #4</div>
+          <div class="col-xs-36 col-md-9">Colour #5</div>
+          <div class="col-xs-36 col-md-9">Colour #6</div>
+          <div class="col-xs-36 col-md-9">Colour #7</div>
+          <div class="col-xs-36 col-md-9">Colour #8</div>
+          <div class="col-xs-36 col-md-9">Colour #9</div>
+          <div class="col-xs-36 col-md-9">Colour #10</div>
+        </div>
 
       </div>
 
     </Layout>
+    </body>
 
   );
 };
