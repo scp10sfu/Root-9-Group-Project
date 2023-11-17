17fdc46944b00533f31ad18fe9b5049b24dca867
CindyXiao1 <cindyxiaoyi123@gmail.com>
2023-11-17 00:25:31 -0800

    changed dark mode and light mode
    added icons to it too

--- colour-palette/src/App.js
+++ colour-palette/src/App.js
@@ -3,25 +3,25 @@
  * App.js
  * @component
  */
-import React, { createContext, useState } from 'react';
-import './App.css';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import About from './Pages/About';
-import Home from './Pages/Home';
-import ColourPicker from './Pages/ColorPicker';
-import PaletteGenerator from './Pages/PaletteGenerator';
-import MoodboardGenerator from './Pages/MoodboardGenerator';
-import NotFoundPage from './Pages/NotFoundPage';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import ColorSwitcher from './Components/ColorSwitcher';
-import NavigationBar from './Components/NavigationBar';
+import React, { createContext, useState, useMemo } from "react";
+import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import ColourPicker from "./Pages/ColorPicker";
+import PaletteGenerator from "./Pages/PaletteGenerator";
+import MoodboardGenerator from "./Pages/MoodboardGenerator";
+import NotFoundPage from "./Pages/NotFoundPage";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import ColorSwitcher from "./Components/ColorSwitcher";
+import NavigationBar from "./Components/NavigationBar";
 
 // TODO: add theme switcher to nav bar component
 
 /*  navigation bar
  *  | Home (icon + title) | About | Colour Picker | Palette Generator | Moodboard Generator |
- *  
+ *
  *  tech menu
  *  | Color Switcher (icon) | Export (icon) |
  */
@@ -29,55 +29,67 @@ import NavigationBar from './Components/NavigationBar';
 export const ColorContext = createContext(); // Create ColorContext
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+  const themeValue = useMemo(() => ({ theme, setTheme }), [theme]);
 
   // Router
   const navigate = useNavigate(); // gets navigation and stores it
-  const navigateToHome = () => { navigate('/'); };
-  const navigateToAbout = () => { navigate('/About'); };
-  const navigateToColourPicker = () => { navigate('/ColourPicker'); };
-  const navigateToPaletteGenerator = () => { navigate('/PaletteGenerator'); };
-  const navigateToMoodboardGenerator = () => { navigate('/MoodboardGenerator'); };
-  const navigateToNotFoundPage = () => { navigate('/NotFoundPage'); };
-  const navigateToModeSwitch = () => { navigate('/ModeSwitch'); };
-  
+  const navigateToHome = () => {
+    navigate("/");
+  };
+  const navigateToAbout = () => {
+    navigate("/About");
+  };
+  const navigateToColourPicker = () => {
+    navigate("/ColourPicker");
+  };
+  const navigateToPaletteGenerator = () => {
+    navigate("/PaletteGenerator");
+  };
+  const navigateToMoodboardGenerator = () => {
+    navigate("/MoodboardGenerator");
+  };
+  const navigateToNotFoundPage = () => {
+    navigate("/NotFoundPage");
+  };
+  const navigateToModeSwitch = () => {
+    navigate("/ModeSwitch");
+  };
+
   return (
+    <ColorContext.Provider value={themeValue}>
+      <div className={`App ${theme === "dark" ? "dark" : ""}`}>
+        {/* Include the NavigationBar component */}
+        <NavigationBar />
 
-    <div className="App">
+        {/* TODO: create nav bar as a component */}
+        {/* <Header /> */}
 
-      {/* Include the NavigationBar component */}
-      <NavigationBar />
-
-      {/* <ColorSwitcher /> */}
-
-      {/* TODO: create nav bar as a component */}
-      {/* <Header /> */}
-
-
-      <div>
         <div>
-          <Routes>
-            <Route path="/About" element={<About/>} />
-            <Route path="/" element={<Home />} />
-            <Route path="/ColourPicker" element={<ColourPicker />} />
-            <Route path="/PaletteGenerator" element={<PaletteGenerator />} />
-            <Route path="/MoodboardGenerator" element={<MoodboardGenerator />} />
-          </Routes>
+          <div>
+            <Routes>
+              <Route path="/About" element={<About />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/ColourPicker" element={<ColourPicker />} />
+              <Route path="/PaletteGenerator" element={<PaletteGenerator />} />
+              <Route
+                path="/MoodboardGenerator"
+                element={<MoodboardGenerator />}
+              />
+            </Routes>
+          </div>
         </div>
-      </div> 
 
-      <main className="app-content">
-
-        {/* 
+        <main className="app-content">
+          {/*
         <header className="text_block">
         <h1>Color Picker</h1>
         <p>Extract wonderful palettes from your image.</p>
         </header> */}
-
-      </main>
-    </div>
+        </main>
+      </div>
+    </ColorContext.Provider>
   );
 };
-
-
 
 export default App;
--- colour-palette/src/Components/NavigationBar.js
+++ colour-palette/src/Components/NavigationBar.js
@@ -1,52 +1,57 @@
 // NavigationBar.js
 //import "./App.css";
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { createContext, useState } from 'react';
-import './NavigationBar.css';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { createContext, useState } from "react";
+import "./NavigationBar.css";
 
-import About from '../Pages/About';
-import Home from '../Pages/Home';
-import ColourPicker from '../Pages/ColorPicker';
-import PaletteGenerator from '../Pages/PaletteGenerator';
-import MoodboardGenerator from '../Pages/MoodboardGenerator';
+import About from "../Pages/About";
+import Home from "../Pages/Home";
+import ColourPicker from "../Pages/ColorPicker";
+import PaletteGenerator from "../Pages/PaletteGenerator";
+import MoodboardGenerator from "../Pages/MoodboardGenerator";
+import ColorSwitcher from "./ColorSwitcher";
 
-export const ThemeContext = createContext(null);
-function App(){
-  const[theme, setTheme] = useState("light");
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
-  };
-  return(
-      <ThemeContext.Provider value = {{theme, setTheme}}>
-        <div className= "App" id={theme}>
-        </div>
-      </ThemeContext.Provider>
-
-  )
-
-}
 const NavigationBar = () => {
   // Router
   const navigate = useNavigate();
-  const navigateToHome = () => { navigate('/'); };
-  const navigateToAbout = () => { navigate('/About'); };
-  const navigateToColourPicker = () => { navigate('/ColourPicker'); };
-  const navigateToPaletteGenerator = () => { navigate('/PaletteGenerator'); };
-  const navigateToMoodboardGenerator = () => { navigate('/MoodboardGenerator'); };
-  const navigateToNotFoundPage = () => { navigate('/NotFoundPage'); };
+  const navigateToHome = () => {
+    navigate("/");
+  };
+  const navigateToAbout = () => {
+    navigate("/About");
+  };
+  const navigateToColourPicker = () => {
+    navigate("/ColourPicker");
+  };
+  const navigateToPaletteGenerator = () => {
+    navigate("/PaletteGenerator");
+  };
+  const navigateToMoodboardGenerator = () => {
+    navigate("/MoodboardGenerator");
+  };
+  const navigateToNotFoundPage = () => {
+    navigate("/NotFoundPage");
+  };
 
   return (
     <nav className="nav-bar">
-      <div className='nav-content'>
-        <button className="title-page" onClick={navigateToHome}>Title</button>
+      <div className="nav-content">
+        <button className="title-page" onClick={navigateToHome}>
+          Title
+        </button>
         <div className="links">
           {/* <button onClick={navigateToHome}>Home</button> */}
           <button onClick={navigateToAbout}>About</button>
           <button onClick={navigateToColourPicker}>Colour Picker</button>
-          <button onClick={navigateToPaletteGenerator}>Palette Generator</button>
-          <button onClick={navigateToMoodboardGenerator}>Moodboard Generator</button>
+          <button onClick={navigateToPaletteGenerator}>
+            Palette Generator
+          </button>
+          <button onClick={navigateToMoodboardGenerator}>
+            Moodboard Generator
+          </button>
         </div>
+        <ColorSwitcher />
       </div>
     </nav>
   );
