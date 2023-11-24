8883ca7bc4f9d0339b14f70c288c93d3113cad9a
Taiga Okuma <toa12@sfu.ca>
2023-11-23 19:25:29 -0800

    Added responce message from ChatGPT

--- colour-palette/src/Pages/PaletteGenerator.js
+++ colour-palette/src/Pages/PaletteGenerator.js
@@ -1,75 +1,143 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import "./PaletteGenerator.css";
+
+// function PaletteGenerator() {
+//   const [colors, setColors] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false); // Move this inside the component
+
+//   useEffect(() => {
+//     const form = document.getElementById("paletteForm");
+//     const colorSwatches = document.getElementById("colorSwatches");
+
+//     form.addEventListener("submit", async (e) => {
+//       e.preventDefault();
+//       const prompt = document.getElementById("prompt").value;
+
+//       // Set loading to true
+//       setIsLoading(true);
+
+//       try {
+//         const response = await axios.post("/get_palette", { prompt });
+//         const { data } = response;
+
+//         // Clear previous colors
+//         while (colorSwatches.firstChild) {
+//           colorSwatches.removeChild(colorSwatches.firstChild);
+//         }
+
+//         if (data.colors.length > 0) {
+//           data.colors.forEach((color) => {
+//             const colorSwatch = document.createElement("div");
+//             colorSwatch.className = "color-swatch";
+//             colorSwatch.style.backgroundColor = color;
+//             colorSwatches.appendChild(colorSwatch);
+//           });
+//         } else {
+//           const errorMessage = document.createElement("p");
+//           errorMessage.textContent = "No HEX color codes could be identified.";
+//           colorSwatches.appendChild(errorMessage);
+//         }
+
+//         setColors(data.colors);
+//       } catch (error) {
+//         console.error("Error:", error);
+//       } finally {
+//         // Set loading to false regardless of the outcome
+//         setIsLoading(false);
+//       }
+//     });
+//   }, []);
+
+//   return (
+//     <div className="palette-generator">
+//       <h2>Ask for a Color Palette</h2>
+//       <form id="paletteForm">
+//         <label htmlFor="prompt">Describe the picture you want to draw:</label>
+//         <br />
+//         <input type="text" id="prompt" name="prompt" required />
+//         <br />
+//         <input type="submit" value="Get Palette" />
+//       </form>
+//       {isLoading && (
+//         <div className="spinner-container">
+//           <div className="spinner"></div>
+//         </div>
+//       )}
+//       <div id="palette">
+//         <p>Color Palette:</p>
+//         <div className="color-swatches" id="colorSwatches"></div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default PaletteGenerator;
+import React, { useState } from "react";
 import axios from "axios";
 import "./PaletteGenerator.css";
 
 function PaletteGenerator() {
-  const [colors, setColors] = useState([]);
-  const [isLoading, setIsLoading] = useState(false); // Move this inside the component
+    const [prompt, setPrompt] = useState('');
+    const [colors, setColors] = useState([]);
+    const [fullResponse, setFullResponse] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const form = document.getElementById("paletteForm");
-    const colorSwatches = document.getElementById("colorSwatches");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        setFullResponse(''); // Clear previous response
 
-    form.addEventListener("submit", async (e) => {
-      e.preventDefault();
-      const prompt = document.getElementById("prompt").value;
-
-      // Set loading to true
-      setIsLoading(true);
-
-      try {
-        const response = await axios.post("/get_palette", { prompt });
-        const { data } = response;
-
-        // Clear previous colors
-        while (colorSwatches.firstChild) {
-          colorSwatches.removeChild(colorSwatches.firstChild);
+        try {
+            const response = await axios.post("/get_palette", { prompt });
+            setColors(response.data.colors || []);
+            setFullResponse(response.data.fullResponse || ""); // Update with full response
+        } catch (error) {
+            console.error("Error:", error);
+            setFullResponse('Failed to get the color palette. Please try again.');
+        } finally {
+            setIsLoading(false);
+            setPrompt(''); // Reset the prompt
         }
+    };
 
-        if (data.colors.length > 0) {
-          data.colors.forEach((color) => {
-            const colorSwatch = document.createElement("div");
-            colorSwatch.className = "color-swatch";
-            colorSwatch.style.backgroundColor = color;
-            colorSwatches.appendChild(colorSwatch);
-          });
-        } else {
-          const errorMessage = document.createElement("p");
-          errorMessage.textContent = "No HEX color codes could be identified.";
-          colorSwatches.appendChild(errorMessage);
-        }
-
-        setColors(data.colors);
-      } catch (error) {
-        console.error("Error:", error);
-      } finally {
-        // Set loading to false regardless of the outcome
-        setIsLoading(false);
-      }
-    });
-  }, []);
-
-  return (
-    <div className="palette-generator">
-      <h2>Ask for a Color Palette</h2>
-      <form id="paletteForm">
-        <label htmlFor="prompt">Describe the picture you want to draw:</label>
-        <br />
-        <input type="text" id="prompt" name="prompt" required />
-        <br />
-        <input type="submit" value="Get Palette" />
-      </form>
-      {isLoading && (
-        <div className="spinner-container">
-          <div className="spinner"></div>
+    return (
+        <div className="palette-generator">
+            <h2>Ask for a Color Palette</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="prompt">
+                    Describe the picture you want to draw:
+                </label>
+                <br />
+                <input
+                    type="text"
+                    id="prompt"
+                    name="prompt"
+                    value={prompt}
+                    onChange={e => setPrompt(e.target.value)}
+                    required
+                />
+                <br />
+                <input type="submit" value="Get Palette" />
+            </form>
+            {isLoading && (
+                <div className="spinner-container">
+                    <div className="spinner"></div>
+                </div>
+            )}
+            <div id="palette-response">
+                {fullResponse && <p className="palette-response-text">{fullResponse}</p>}
+            </div>
+            <div id="palette">
+                <div className="color-swatches">
+                    {colors.map((color, index) => (
+                        <div key={index} className="color-swatch" style={{ backgroundColor: color }}></div>
+                    ))}
+                </div>
+            </div>
         </div>
-      )}
-      <div id="palette">
-        <p>Color Palette:</p>
-        <div className="color-swatches" id="colorSwatches"></div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default PaletteGenerator;
+
--- colour-palette/src/server/app.py
+++ colour-palette/src/server/app.py
@@ -1,3 +1,70 @@
+
+
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import os
+# import openai
+# import re
+
+# # Load environment variables
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# # Initialize Flask app
+# app = Flask(__name__)
+# CORS(app)  # Enable CORS for all routes
+# app.debug = True  # Ensure debug mode is on
+
+# # Initialize OpenAI with the API key from the .env file
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# # Define a regular expression pattern to match HEX color codes
+# HEX_COLOR_PATTERN = r'(#[0-9A-Fa-f]{6}\b)'
+
+# @app.route('/get_palette', methods=['POST'])
+# def get_palette():
+#     user_prompt = request.json['prompt'] + " Provide a color palette with HEX codes."
+
+#     # Log the received prompt
+#     print(f"Received prompt: {user_prompt}")
+
+#     try:
+#         # Call the OpenAI API
+#         response = openai.chat.completions.create(
+#             model="gpt-3.5-turbo",
+#             messages=[
+#                 {"role": "system", "content": "You are a helpful assistant."},
+#                 {"role": "user", "content": user_prompt}
+#             ]
+#         )
+    
+#         # Log the entire response content
+#         print(f"OpenAI API response: {response.choices[0].message.content}")
+
+#         # Extract the response content
+#         content = response.choices[0].message.content.strip()
+
+#         # Attempt to find HEX codes in the response
+#         hex_codes = re.findall(HEX_COLOR_PATTERN, content, re.IGNORECASE)
+
+#         # Log the found HEX codes
+#         print(f"Extracted HEX codes: {hex_codes}")
+
+#         if hex_codes:
+#             # Send all unique HEX codes
+#             unique_hex_codes = list(set(hex_codes))
+#             return jsonify({"colors": unique_hex_codes})
+#         else:
+#             return jsonify({"error": "No HEX color codes could be identified."}), 200
+
+#     except Exception as e:
+#         # Log the exception
+#         print(f"An error occurred: {e}")
+#         return jsonify({"error": str(e)}), 500
+
+# # Run the Flask app
+# if __name__ == '__main__':
+#     app.run()
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
@@ -11,6 +78,7 @@ load_dotenv()
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+app.debug = True  # Ensure debug mode is on
 
 # Initialize OpenAI with the API key from the .env file
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -18,14 +86,57 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Define a regular expression pattern to match HEX color codes
 HEX_COLOR_PATTERN = r'(#[0-9A-Fa-f]{6}\b)'
 
+# @app.route('/get_palette', methods=['POST'])
+# def get_palette():
+#     user_prompt = request.json['prompt'] + " Provide a color palette with HEX codes."
+    
+#     # Log the received prompt
+#     print(f"Received prompt: {user_prompt}")
+
+#     try:
+#         # Call the OpenAI API
+#         response = openai.chat.completions.create(
+#             model="gpt-3.5-turbo",
+#             messages=[
+#                 {"role": "system", "content": "You are a helpful assistant."},
+#                 {"role": "user", "content": user_prompt}
+#             ]
+#         )
+
+#         # Extract the response content
+#         content = response.choices[0].message.content.strip()
+
+#         # Log the entire response content
+#         print(f"OpenAI API response: {response.choices[0].message.content}")
+
+#         # Attempt to find HEX codes in the response
+#         hex_codes = re.findall(HEX_COLOR_PATTERN, content, re.IGNORECASE)
+
+#         # Log the found HEX codes
+#         print(f"Extracted HEX codes: {hex_codes}")
+
+#         if hex_codes:
+#             # Send all unique HEX codes along with a custom message
+#             unique_hex_codes = list(set(hex_codes))
+#             return jsonify({
+#                 "colors": unique_hex_codes, 
+#                 "message": "Your customized color palette:"
+#             })
+#         else:
+#             return jsonify({"error": "No HEX color codes could be identified."}), 200
+
+#     except Exception as e:
+#         # Log the exception
+#         app.logger.error(f"An error occurred: {e}")
+#         return jsonify({"error": str(e)}), 500
 @app.route('/get_palette', methods=['POST'])
 def get_palette():
-    user_prompt = request.json.get('prompt') + " Provide a color palette with HEX codes."
+    user_prompt = request.json['prompt'] + " Provide a color palette with HEX codes."
 
     try:
         # Call the OpenAI API
         response = openai.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
+            model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": user_prompt}
@@ -33,21 +144,26 @@ def get_palette():
         )
 
         # Extract the response content
-        content = response.choices[0].message.content.strip()
+        full_response = response.choices[0].message.content.strip()
+        print(f"OpenAI API response: {full_response}")
 
         # Attempt to find HEX codes in the response
-        hex_codes = re.findall(HEX_COLOR_PATTERN, content, re.IGNORECASE)
+        hex_codes = re.findall(HEX_COLOR_PATTERN, full_response, re.IGNORECASE)
 
         if hex_codes:
-            return jsonify({"colors": hex_codes})
+            # Send all unique HEX codes and the full response
+            unique_hex_codes = list(set(hex_codes))
+            return jsonify({
+                "colors": unique_hex_codes, 
+                "fullResponse": full_response  # Include the full response text
+            })
         else:
             return jsonify({"error": "No HEX color codes could be identified."}), 200
 
     except Exception as e:
-        # Log the exception
-        app.logger.error(f"An error occurred: {e}")
+        print(f"An error occurred: {e}")
         return jsonify({"error": str(e)}), 500
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
