e4a829abc9b89d0397d757982acb0e91627a6ee2
Taiga Okuma <toa12@sfu.ca>
2023-11-24 03:20:18 -0800

    Added server.js which is node.js

--- colour-palette/src/Pages/PaletteGenerator.js
+++ colour-palette/src/Pages/PaletteGenerator.js
@@ -1,78 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import "./PaletteGenerator.css";
-
-// function PaletteGenerator() {
-//   const [colors, setColors] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false); // Move this inside the component
-
-//   useEffect(() => {
-//     const form = document.getElementById("paletteForm");
-//     const colorSwatches = document.getElementById("colorSwatches");
-
-//     form.addEventListener("submit", async (e) => {
-//       e.preventDefault();
-//       const prompt = document.getElementById("prompt").value;
-
-//       // Set loading to true
-//       setIsLoading(true);
-
-//       try {
-//         const response = await axios.post("/get_palette", { prompt });
-//         const { data } = response;
-
-//         // Clear previous colors
-//         while (colorSwatches.firstChild) {
-//           colorSwatches.removeChild(colorSwatches.firstChild);
-//         }
-
-//         if (data.colors.length > 0) {
-//           data.colors.forEach((color) => {
-//             const colorSwatch = document.createElement("div");
-//             colorSwatch.className = "color-swatch";
-//             colorSwatch.style.backgroundColor = color;
-//             colorSwatches.appendChild(colorSwatch);
-//           });
-//         } else {
-//           const errorMessage = document.createElement("p");
-//           errorMessage.textContent = "No HEX color codes could be identified.";
-//           colorSwatches.appendChild(errorMessage);
-//         }
-
-//         setColors(data.colors);
-//       } catch (error) {
-//         console.error("Error:", error);
-//       } finally {
-//         // Set loading to false regardless of the outcome
-//         setIsLoading(false);
-//       }
-//     });
-//   }, []);
-
-//   return (
-//     <div className="palette-generator">
-//       <h2>Ask for a Color Palette</h2>
-//       <form id="paletteForm">
-//         <label htmlFor="prompt">Describe the picture you want to draw:</label>
-//         <br />
-//         <input type="text" id="prompt" name="prompt" required />
-//         <br />
-//         <input type="submit" value="Get Palette" />
-//       </form>
-//       {isLoading && (
-//         <div className="spinner-container">
-//           <div className="spinner"></div>
-//         </div>
-//       )}
-//       <div id="palette">
-//         <p>Color Palette:</p>
-//         <div className="color-swatches" id="colorSwatches"></div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default PaletteGenerator;
 import React, { useState } from "react";
 import axios from "axios";
 import "./PaletteGenerator.css";
@@ -87,9 +12,11 @@ function PaletteGenerator() {
         e.preventDefault();
         setIsLoading(true);
         setFullResponse(''); // Clear previous response
+        setColors([]); // Clear previous colors
 
         try {
-            const response = await axios.post("/get_palette", { prompt });
+            // Update this URL to point to your Node.js server endpoint
+            const response = await axios.post("http://localhost:3000/get_palette", { prompt });
             setColors(response.data.colors || []);
             setFullResponse(response.data.fullResponse || ""); // Update with full response
         } catch (error) {
@@ -97,7 +24,6 @@ function PaletteGenerator() {
             setFullResponse('Failed to get the color palette. Please try again.');
         } finally {
             setIsLoading(false);
-            setPrompt(''); // Reset the prompt
         }
     };
 
@@ -118,7 +44,7 @@ function PaletteGenerator() {
                     required
                 />
                 <br />
-                <input type="submit" value="Get Palette" />
+                <input type="submit" value="Get Palette" disabled={isLoading} />
             </form>
             {isLoading && (
                 <div className="spinner-container">
@@ -129,15 +55,16 @@ function PaletteGenerator() {
                 {fullResponse && <p className="palette-response-text">{fullResponse}</p>}
             </div>
             <div id="palette">
-                <div className="color-swatches">
-                    {colors.map((color, index) => (
-                        <div key={index} className="color-swatch" style={{ backgroundColor: color }}></div>
-                    ))}
-                </div>
+                {colors.length > 0 && (
+                    <div className="color-swatches">
+                        {colors.map((color, index) => (
+                            <div key={index} className="color-swatch" style={{ backgroundColor: color }}></div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
 }
 
 export default PaletteGenerator;
-
