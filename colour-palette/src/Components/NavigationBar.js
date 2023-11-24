3e8ec57f28a82a8560a3c7400b3ced1c2e6328b9
Anna Rusinova <ara103@sfu.ca>
2023-11-23 17:01:09 -0800

    feat: update the appearance of the mobile menu

--- colour-palette/src/Components/NavigationBar.js
+++ colour-palette/src/Components/NavigationBar.js
@@ -5,6 +5,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import { createContext, useState } from 'react';
 import './NavigationBar.css';
 import { ReactComponent as CloseIcon } from '../images/icon-close-white.svg';
+import { ReactComponent as MenuIcon } from '../images/icon-menu-dark.svg';
 
 import About from '../Pages/About';
 import Home from '../Pages/Home';
@@ -29,206 +30,6 @@ function App() {
   )
 }
 
-
-// const NavigationBar = () => {
-//   // Router
-//   const navigate = useNavigate();
-//   const navigateToHome = () => { navigate('/'); };
-//   const navigateToAbout = () => { navigate('/About'); };
-//   const navigateToColourExtractor = () => { navigate('/ColourExtractor'); };
-//   const navigateToPaletteGenerator = () => { navigate('/PaletteGenerator'); };
-//   const navigateToMoodboardGenerator = () => { navigate('/MoodboardGenerator'); };
-//   const navigateToNotFoundPage = () => { navigate('/NotFoundPage'); };
-
-//   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-//   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       const newIsMobile = window.innerWidth <= 768;
-
-//       // Close the mobile menu if it's open and the screen width exceeds the threshold
-//       if (isMobileMenuOpen && !newIsMobile) {
-//         setMobileMenuOpen(false);
-//       }
-
-//       setIsMobile(newIsMobile);
-//     };
-
-//     window.addEventListener('resize', handleResize);
-
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//     };
-//   }, [isMobileMenuOpen]);
-
-//   const toggleMobileMenu = () => {
-//     setMobileMenuOpen(!isMobileMenuOpen);
-//   };
-
-//   const renderLinks = () => {
-//     if (isMobile) {
-// //       return (
-// //         <>
-// //         <div class="hero">
-// //   <h1>Fullscreen Menu</h1>
-// //   <div class="menu-btn">
-// //     <span></span>
-// //     <span></span>
-
-// //   </div>
-// // </div> 
-
-// //         <div className="menu active">
-// //           <span className="btn-close">&times;</span>
-// //           <ul className="links-container">
-// //             {/* <li className="menu-title">MENU</li> */}
-// //             <li>
-// //               <a href="" className="home">HOME</a>
-// //               <button href="" className="home" onClick={navigateToHome}>HOME</button>
-// //             </li>
-// //             <li>
-// //               {/* <a href="" className="places">PLACES</a> */}
-// //               <button href="" className="about" onClick={navigateToAbout}>ABOUT</button>
-// //             </li>
-// //             <li>
-// //               <a href="" className="about">ABOUT</a>
-// //             </li>
-// //             <li>
-// //               <a href="" className="contact">CONTACT</a>
-
-// //             </li>
-
-// //           </ul>
-// //         </div>
-// //         </>
-// <>
-// <div class="button_container" id="toggle"><span class="top"></span><span class="middle"></span><span class="bottom"></span></div>
-// <div class="overlay" id="overlay">
-//     <nav class="overlay-menu">
-//         <ul>
-//             <li><a href="#">Home</a></li>
-//             <li><a href="#">About</a></li>
-//             <li><a href="#">Work</a></li>
-//             <li><a href="#">Contact</a></li>
-//         </ul>
-//     </nav>
-// </div>
-// </>
-//       // );
-//     } else {
-//       return (
-//         <nav className="nav-bar">
-//           <div className='nav-content'>
-//         <div className="links">
-//           <button onClick={navigateToAbout}>About</button>
-//           <button onClick={navigateToColourExtractor}>Colour Extractor</button>
-//           <button onClick={navigateToPaletteGenerator}>Palette Generator</button>
-//           <button onClick={navigateToMoodboardGenerator}>Moodboard Generator</button>
-//         </div>
-//         </div>
-//         </nav>
-//       );
-//     }
-//   };
-
-//   // return (
-//   //   <nav className={`nav-bar ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
-//   //     <div className='nav-content'>
-//   //       <button className="title-page" onClick={navigateToHome}>Paletä</button>
-
-//   //       <div className={`menu-icon ${isMobileMenuOpen ? 'close-icon' : ''}`} onClick={toggleMobileMenu}>
-//   //         {/* Hamburger icon or close icon */}
-//   //         <div className="bar"></div>
-//   //         <div className="bar"></div>
-//   //         <div className="bar"></div>
-//   //       </div>
-
-//   //       <div className={`links ${isMobileMenuOpen ? 'mobile-links' : ''}`}>
-//   //         <button onClick={navigateToAbout}>About</button>
-//   //         <button onClick={navigateToColourExtractor}>Colour Extractor</button>
-//   //         <button onClick={navigateToPaletteGenerator}>Palette Generator</button>
-//   //         <button onClick={navigateToMoodboardGenerator}>Moodboard Generator</button>
-//   //       </div>
-
-//         {/* <div className="links">
-//           <button onClick={navigateToAbout}>About</button>
-//           <button onClick={navigateToColourExtractor}>Colour Extractor</button>
-//           <button onClick={navigateToPaletteGenerator}>Palette Generator</button>
-//           <button onClick={navigateToMoodboardGenerator}>Moodboard Generator</button>
-        
-//           <div class="menu-toggle">
-//           <ion-icon name="menu"></ion-icon>
-//         </div>
-//         <div class="overlay">
-//           <div class="close-btn">
-//             <ion-icon name="close"></ion-icon>
-//           </div>
-//           <div class="menu">
-//             <ul>
-//               <li>
-//                 <a href="#home">Home</a>
-//               </li>
-//               <li>
-//                 <a href="#about">About</a>
-//               </li>
-//               <li>
-//                 <a href="#projects">Projects</a>
-//               </li>
-//               <li>
-//                 <a href="#contact">Contact</a>
-//               </li>
-//             </ul>
-//           </div>
-//         </div> */}
-
-
-// //         return (
-// //           <nav className={`nav-bar ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
-// //             <div className='nav-content'>
-// //               <button className="title-page" onClick={navigateToHome}>Paletä</button>
-// //               {renderLinks()}
-    
-// //         <ColorSwitcher />
-
-        
-// //       </div>
-// //     </nav>
-// //   );
-// // };
-
-
-
-// return (
-// <nav className="nav-bar">
-//       <div className="nav-content">
-//         <button className="title-page" onClick={() => { navigate('/'); closeMobileMenu(); }}>Paletä</button>
-
-//         {isMobile ? (
-//           <>
-//             <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
-//               {isMobileMenuOpen ? 'Close' : 'Menu'}
-//             </button>
-//             {isMobileMenuOpen && <MobileMenu closeMenu={closeMobileMenu} />}
-//           </>
-//         ) : (
-//           <div className="links">
-//             <button onClick={() => { navigate('/About'); closeMobileMenu(); }}>About</button>
-//             <button onClick={() => { navigate('/ColourExtractor'); closeMobileMenu(); }}>Colour Extractor</button>
-//             <button onClick={() => { navigate('/PaletteGenerator'); closeMobileMenu(); }}>Palette Generator</button>
-//             <button onClick={() => { navigate('/MoodboardGenerator'); closeMobileMenu(); }}>Moodboard Generator</button>
-//           </div>
-//         )}
-
-//         <ColorSwitcher />
-//       </div>
-//     </nav>
-//   );
-// };
-
-
-// export default NavigationBar;
-
 const MobileMenu = ({ closeMenu }) => {
   const navigate = useNavigate();
 
@@ -249,11 +50,11 @@ const MobileMenu = ({ closeMenu }) => {
 
 const NavigationBar = () => {
   const navigate = useNavigate();
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 800);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const handleResize = () => {
-    const newIsMobile = window.innerWidth <= 800;
+    const newIsMobile = window.innerWidth <= 768;
 
     if (isMobileMenuOpen && !newIsMobile) {
       setMobileMenuOpen(false);
@@ -285,35 +86,42 @@ const NavigationBar = () => {
 
         {isMobile && (
           <>
+            {/* <ColorSwitcher /> */}
             <button className="mobile-menu-btn" onClick={toggleMobileMenu} style={{ zIndex: isMobileMenuOpen ? 1001 : 1000, color: isMobileMenuOpen ? 'white' : 'black' }}>
               {isMobileMenuOpen ? <CloseIcon /> : 'Menu'}
+              {/* { isMobileMenuOpen ? <CloseIcon /> : <MenuIcon /> } */}
             </button>
-          
+
             {isMobileMenuOpen && (
 
-            <div className="mobile-menu-overlay">
-              <div className="mobile-menu-content">
-              <button onClick={() => { navigate('/About'); closeMobileMenu(); }}>About</button>
-            <button onClick={() => { navigate('/ColourExtractor'); closeMobileMenu(); }}>Colour Extractor</button>
-            <button onClick={() => { navigate('/PaletteGenerator'); closeMobileMenu(); }}>Palette Generator</button>
-            <button onClick={() => { navigate('/MoodboardGenerator'); closeMobileMenu(); }}>Moodboard Generator</button>
+              <div className="mobile-menu-overlay">
+                <div className="mobile-menu-content">
+                  <button onClick={() => { navigate('/About'); closeMobileMenu(); }}>About</button>
+                  <button onClick={() => { navigate('/ColourExtractor'); closeMobileMenu(); }}>Colour Extractor</button>
+                  <button onClick={() => { navigate('/PaletteGenerator'); closeMobileMenu(); }}>Palette Generator</button>
+                  <button onClick={() => { navigate('/MoodboardGenerator'); closeMobileMenu(); }}>Moodboard Generator</button>
+                </div>
               </div>
-            </div>
             )}
-             {isMobileMenuOpen && <MobileMenu closeMenu={closeMobileMenu} />}
+            {isMobileMenuOpen && <MobileMenu closeMenu={closeMobileMenu} />}
           </>
         )}
 
         {!isMobile && (
-          <div className="links">
-            <button onClick={() => { navigate('/About'); closeMobileMenu(); }}>About</button>
-            <button onClick={() => { navigate('/ColourExtractor'); closeMobileMenu(); }}>Colour Extractor</button>
-            <button onClick={() => { navigate('/PaletteGenerator'); closeMobileMenu(); }}>Palette Generator</button>
-            <button onClick={() => { navigate('/MoodboardGenerator'); closeMobileMenu(); }}>Moodboard Generator</button>
-          </div>
+          <>
+            <div className="links">
+              <button onClick={() => { navigate('/About'); closeMobileMenu(); }}>About</button>
+              <button onClick={() => { navigate('/ColourExtractor'); closeMobileMenu(); }}>Colour Extractor</button>
+              <button onClick={() => { navigate('/PaletteGenerator'); closeMobileMenu(); }}>Palette Generator</button>
+              <button onClick={() => { navigate('/MoodboardGenerator'); closeMobileMenu(); }}>Moodboard Generator</button>
+            </div>
+
+            {/* <ColorSwitcher /> */}
+          </>
         )}
 
         <ColorSwitcher />
+
       </div>
     </nav>
   );
