d3adef75daf0b1bba100e86095e76ee2471e86c0
Anna Rusinova <ara103@sfu.ca>
2023-11-25 00:23:04 -0800

    feat: add functions to PaletteGenerator.js

--- colour-palette/src/Pages/PaletteGenerator.js
+++ colour-palette/src/Pages/PaletteGenerator.js
@@ -66,6 +66,198 @@ function PaletteGenerator() {
     }
   };
 
+
+    /**
+  * SkeletonLoader Component
+  * A component representing a skeleton loader with color information.
+  * NOTE: keep this an empty container!
+  * @returns {JSX.Element} - The rendered SkeletonLoader component.
+  */
+    const SkeletonLoader = () => (
+        <>
+          <div className="main-section col-xs-36 col-md-24 grid-container nested-grid">
+            {/* First dominant colour */}
+            <div class="wrapper-2-col secondary-section col-xs-36 col-md-18">
+              <div key={0} className="loader-square-bottom-align" style={{ backgroundColor: firstColor.hex }}>
+                <p className="color-name">{firstColor.name}</p>
+                <p className="color-hex">HEX: {firstColor.hex}</p>
+                <p className="color-rgb">RGB: {firstColor.rgb}</p>
+                <p className="color-cmyk">CMYK: {firstColor.cmyk}</p>
+              </div>
+            </div>
+            {/* Second dominant colour */}
+            <div class="wrapper-2-col secondary-section col-xs-36 col-md-18">
+              <div key={1} className="loader-square-bottom-align" style={{ backgroundColor: secondColor.hex }}>
+                <p className="color-name">{secondColor.name}</p>
+                <p className="color-hex">HEX: {secondColor.hex}</p>
+                <p className="color-rgb">RGB: {secondColor.rgb}</p>
+                <p className="color-cmyk">CMYK: {secondColor.cmyk}</p>
+              </div>
+            </div>
+    
+            <div class="wrapper-4-col secondary-section col-xs-36 col-md-9">
+              <div key={2} className="loader-square-top-align" style={{ backgroundColor: thirdColor.hex }}>
+                <p className="color-name">{thirdColor.name}</p>
+                <p className="color-hex">HEX: {thirdColor.hex}</p>
+                <p className="color-rgb">RGB: {thirdColor.rgb}</p>
+                <p className="color-cmyk">CMYK: {thirdColor.cmyk}</p>
+              </div>
+            </div>
+            <div class="wrapper-4-col secondary-section col-xs-36 col-md-9">
+              <div key={3} className="loader-square-top-align" style={{ backgroundColor: fourthColor.hex }}>
+                <p className="color-name">{fourthColor.name}</p>
+                <p className="color-hex">HEX: {fourthColor.hex}</p>
+                <p className="color-rgb">RGB: {fourthColor.rgb}</p>
+                <p className="color-cmyk">CMYK: {fourthColor.cmyk}</p>
+              </div>
+            </div>
+            <div class="wrapper-4-col secondary-section col-xs-36 col-md-9">
+              <div key={4} className="loader-square-top-align" style={{ backgroundColor: fifthColor.hex }}>
+                <p className="color-name">{fifthColor.name}</p>
+                <p className="color-hex">HEX: {fifthColor.hex}</p>
+                <p className="color-rgb">RGB: {fifthColor.rgb}</p>
+                <p className="color-cmyk">CMYK: {fifthColor.cmyk}</p>
+              </div>
+            </div>
+            <div class="wrapper-4-col secondary-section col-xs-36 col-md-9">
+              <div key={5} className="loader-square-top-align" style={{ backgroundColor: sixthColor.hex }}>
+                <p className="color-name">{sixthColor.name}</p>
+                <p className="color-hex">HEX: {sixthColor.hex}</p>
+                <p className="color-rgb">RGB: {sixthColor.rgb}</p>
+                <p className="color-cmyk">CMYK: {sixthColor.cmyk}</p>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+
+      /**
+  * Default Color Object
+  * Represents a default color with optional properties.
+  */
+  const defaultColor = {
+    name: "Silver",
+    rgba: "196, 196, 196, 0.25"
+    // hex: "#C4C4C4",
+    // rgb: "196, 196, 196",
+    // cmyk: "0, 0, 0, 23.1"
+  };
+
+  /**
+   * ColourBoxBottom Component
+   * A component representing a colour box with color information aligned to bottom.
+   * @param {object} color - The color object.
+   * @returns {JSX.Element} - The rendered ColourBoxBottom component.
+   */
+  const ColourBoxBottom = ({ color }) => {
+    const textColor = getTextColor(color.hex);
+    const [isCopyIconFilled, setIsCopyIconFilled] = useState(false);
+
+    const copyToClipboard = (text) => {
+      navigator.clipboard.writeText(text).then(() => {
+        // toast.success('Copied to clipboard!', { autoClose: 1500 });
+        setShowToast(true);
+        setToastMessage('Copied to clipboard!');
+
+        setTimeout(() => {
+          setShowToast(false);
+        }, 1500); // Auto-close after 2 seconds
+
+        // Change the copy icon to filled for a second
+        setIsCopyIconFilled(true);
+        setTimeout(() => {
+          setIsCopyIconFilled(false);
+        }, 750);
+      });
+    };
+
+    return (
+      <div className="color-bottom-align" style={{ backgroundColor: color.hex }}>
+
+        <div className="color-name-container">
+          <p className="color-name" style={{ color: textColor }}>{color.name}</p>
+
+          <button
+            className="copy-icon"
+            onClick={() => copyToClipboard(`${color.name}\nHEX: ${color.hex}\nRGB: ${color.rgb}\nCMYK: ${color.cmyk}`)}
+            aria-label="Copy to clipboard"
+          >
+            {textColor === 'rgba(18, 18, 18, 1)' ? (
+              isCopyIconFilled ? <CopyIconDarkFilled /> : <CopyIconDarkUnfilled />
+            ) : (
+              isCopyIconFilled ? <CopyIconWhiteFilled /> : <CopyIconWhiteUnfilled />
+            )}
+          </button>
+        </div>
+
+        <p className="color-hex" style={{ color: textColor }}>HEX: {color.hex}</p>
+        <p className="color-rgb" style={{ color: textColor }}>RGB: {color.rgb}</p>
+        <p className="color-cmyk" style={{ color: textColor }}>CMYK: {color.cmyk}</p>
+
+      </div>
+
+    );
+  };
+
+
+  /**
+   * ColourBoxTop Component
+   * A component representing a colour box with color information aligned to top.
+   * @param {object} color - The color object.
+   * @returns {JSX.Element} - The rendered ColourBoxTop component.
+   */
+  const ColourBoxTop = ({ color }) => {
+    const textColor = getTextColor(color.hex);
+    const [isCopyIconFilled, setIsCopyIconFilled] = useState(false);
+
+    const copyToClipboard = (text) => {
+      navigator.clipboard.writeText(text).then(() => {
+        // toast.success('Copied to clipboard!', { autoClose: 1500 });
+        setShowToast(true);
+        setToastMessage('Copied to clipboard!');
+
+        setTimeout(() => {
+          setShowToast(false);
+        }, 1500); // Auto-close after 2 seconds
+
+        // Change the copy icon to filled for a second
+        setIsCopyIconFilled(true);
+        setTimeout(() => {
+          setIsCopyIconFilled(false);
+        }, 750);
+
+      });
+    };
+
+    return (
+      <div className="color-top-align" style={{ backgroundColor: color.hex }}>
+
+        <div className="color-name-container">
+          <p className="color-name" style={{ color: textColor }}>{color.name}</p>
+
+          <button
+            className="copy-icon"
+            onClick={() => copyToClipboard(`${color.name}\nHEX: ${color.hex}\nRGB: ${color.rgb}\nCMYK: ${color.cmyk}`)}
+            aria-label="Copy to clipboard"
+          >
+            {textColor === 'rgba(18, 18, 18, 1)' ? (
+              isCopyIconFilled ? <CopyIconDarkFilled /> : <CopyIconDarkUnfilled />
+            ) : (
+              isCopyIconFilled ? <CopyIconWhiteFilled /> : <CopyIconWhiteUnfilled />
+            )}
+          </button>
+        </div>
+
+        <p className="color-hex" style={{ color: textColor }}>HEX: {color.hex}</p>
+        <p className="color-rgb" style={{ color: textColor }}>RGB: {color.rgb}</p>
+        <p className="color-cmyk" style={{ color: textColor }}>CMYK: {color.cmyk}</p>
+
+      </div>
+    );
+  };
+
+  
+
   /* ************************************************************************ */
   /* ************************************************************************ */
   /* ************************************************************************ */
