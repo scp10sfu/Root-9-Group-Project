4df486ca828c2432155a5ee0cf6f8ba0e194a446
Taiga Okuma <toa12@sfu.ca>
2023-11-26 14:31:01 -0800

    Added favicon

--- colour-palette/src/Components/Footer.js
+++ colour-palette/src/Components/Footer.js
@@ -4,13 +4,13 @@ import React from 'react';
 import "../Components/Footer.css"
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className="footer-container">
-      <img src="PaletaLogo.png" alt="Paleta Logo" height="80px" width="200px"/>
-      </div>
-    </footer>
-  );
+  // return (
+  //   <footer>
+  //     <div className="footer-container">
+  //     <img src="PaletaLogo.png" alt="Paleta Logo" height="80px" width="200px"/>
+  //     </div>
+  //   </footer>
+  // );
 };
 
 export default Footer;
