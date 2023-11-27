f9f205d464192521b75e4f52e394462353aa60bb
Anna Rusinova <112856403+arusinova@users.noreply.github.com>
2023-11-26 16:55:50 -0800

    feat: delete userRef import

--- colour-palette/src/Pages/PaletteGenerator.js
+++ colour-palette/src/Pages/PaletteGenerator.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ColorThief from 'colorthief';
 import './PaletteGenerator.css';
