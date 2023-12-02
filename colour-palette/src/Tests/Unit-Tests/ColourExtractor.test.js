c7a5f93b59c19259daf421eea923a3145d6083a2
scp10sfu <scp10@sfu.ca>
2023-12-02 11:34:02 -0800

    Updated unit tests
    Added a new test checking the title and description of the palette generator section, as well as fixing colour extractor unit tests and navigationbar unit tests as both had incorrect methods of testing.

--- colour-palette/src/Tests/Unit-Tests/ColourExtractor.test.js
+++ colour-palette/src/Tests/Unit-Tests/ColourExtractor.test.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import { BrowserRouter } from "react-router-dom";
 import {render, screen} from "@testing-library/react";
 import ColourExtractor from "../../Pages/ColourExtractor";
 import '@testing-library/jest-dom';
 
 
 test('Test for color extractor text', ()=>{
-    render(<BrowserRouter><ColourExtractor /></BrowserRouter>)
-    const title = screen.getByText(/Colour Extractor/i).closest('p');
+    render(<ColourExtractor />)
+    const title = screen.getByText("Colour Extractor").closest('p');
     expect(title).toBeInTheDocument();
 })
 
 test('Test for color extractor text', ()=>{
-    render(<BrowserRouter><ColourExtractor /></BrowserRouter>)
-    const description = screen.getByText(/Extract wonderful palettes from your image./i).closest('p');
+    render(<ColourExtractor />)
+    const description = screen.getByText("Extract wonderful palettes from your image.").closest('p');
     expect(description).toBeInTheDocument();
 })
