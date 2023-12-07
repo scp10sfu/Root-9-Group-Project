65aac302cc47349916f201ca0ee531e026626394
CindyXiao1 <cindyxiaoyi123@gmail.com>
2023-12-07 01:40:09 -0800

    add mocks

--- colour-palette/src/Tests/Footer.test.js
+++ colour-palette/src/Tests/Footer.test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import {render, screen} from "@testing-library/react";
 import Footer from "../Components/Footer";
 import '@testing-library/jest-dom';
@@ -7,4 +7,34 @@ test('Checks if alt text is Paleta Logo', ()=> {
     render(<Footer/>);
     const linkElement = screen.getByAltText(/Paleta Logo/i);
     expect(linkElement).toBeInTheDocument();
+});*/
+
+import React from 'react'
+import {render, screen} from "@testing-library/react";
+import PaletteGenerator from "../Pages/PaletteGenerator";
+import '@testing-library/jest-dom';
+import * as axios from "axios";
+
+// Mock out all top level functions, such as get, put, delete and post:
+jest.mock("axios");
+
+jest.mock('colorthief', () => {
+    return function fn() {return [[1,1,1]];}
 });
+
+test("mock response", () => {
+  axios.post.mockImplementation(() => Promise.resolve({ data: { fullResponse: { "hello": "hello" } } }));
+  render(<PaletteGenerator />)
+});
+
+test('Test for color extractor text' , ()=>{
+    render(<PaletteGenerator />)
+    const title = screen.getByText("AI Palette Generator");
+    expect(title).toBeInTheDocument();
+})
+
+test('Test for color extractor text' , ()=>{
+    render(<PaletteGenerator />)
+    const description = screen.getByText("Generate wonderful palettes.");
+    expect(description).toBeInTheDocument();
+})
