fd51e3e64777009a9f1f6d636f256c43872c96df
Anna Rusinova <ara103@sfu.ca>
2023-12-06 17:17:35 -0800

    feat: add new tests for the About page

--- colour-palette/src/Tests/Unit-Tests/About.test.js
+++ colour-palette/src/Tests/Unit-Tests/About.test.js
@@ -15,7 +15,7 @@ test('Test second line' , () => {
     expect(line2).toBeInTheDocument();
 })
 
-test('Test third line' , () => {
+test('Test overview line' , () => {
     render(<About />)
     const line3 = screen.getByText("Overview");
     expect(line3).toBeInTheDocument();
@@ -25,4 +25,16 @@ test('Test fourth line' , () => {
     render(<About />)
     const line4 = screen.getByText("Our project is designed to cater to visual artists and individuals seeking creative inspiration.");
     expect(line4).toBeInTheDocument();
+})
+
+test('Test main features line' , () => {
+    render(<About />)
+    const line5 = screen.getByText("Main features");
+    expect(line5).toBeInTheDocument();
+})
+
+test('Test contact line' , () => {
+    render(<About />)
+    const line6 = screen.getByText("Contact");
+    expect(line6).toBeInTheDocument();
 })
