3fe978afbc527f95dc5e112b15c5f640a5b32493
Anna Rusinova <ara103@sfu.ca>
2023-12-06 17:13:29 -0800

    feat: correct test cases in About component

--- colour-palette/src/Tests/Unit-Tests/About.test.js
+++ colour-palette/src/Tests/Unit-Tests/About.test.js
@@ -5,24 +5,24 @@ import '@testing-library/jest-dom';
 
 test('Test first line' , () => {
         render(<About />)
-        const line1 = screen.getByText("About Us").closest('p');
+        const line1 = screen.getByText("About Us");
         expect(line1).toBeInTheDocument();
 })
 
 test('Test second line' , () => {
     render(<About />)
-    const line2 = screen.getByText("Get To Know More").closest('p');
+    const line2 = screen.getByText("Get To Know More");
     expect(line2).toBeInTheDocument();
 })
 
 test('Test third line' , () => {
     render(<About />)
-    const line3 = screen.getByText("Overview").closest('p');
+    const line3 = screen.getByText("Overview");
     expect(line3).toBeInTheDocument();
 })
 
 test('Test fourth line' , () => {
     render(<About />)
-    const line4 = screen.getByText("Our project is designed to cater to visual artists and individuals seeking creative inspiration.").closest('p');
+    const line4 = screen.getByText("Our project is designed to cater to visual artists and individuals seeking creative inspiration.");
     expect(line4).toBeInTheDocument();
 })
