5b07b7873141b6d1c934e3f96b29c7e25a87273e
scp10sfu <scp10@sfu.ca>
2023-12-07 02:55:08 -0800

    Another attempt to make toast function test
    Tried creating a utils so the Toast function can be identified as a function.

--- colour-palette/src/Tests/Unit-Tests/PaletteGenerator.test.js
+++ colour-palette/src/Tests/Unit-Tests/PaletteGenerator.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render, screen} from "@testing-library/react";
 import PaletteGenerator from "../../Pages/PaletteGenerator";
 import '@testing-library/jest-dom';
-import {Toast} from '../../Components/Toast';
+import {Toast} from '../toastutils';
 
 jest.mock('colorthief', () => {
     return function fn() {return [[1,1,1]];}
@@ -24,6 +24,6 @@ test('Check the toast message function', () => {
         const Toastmsg = 'info';
         const Toasttype = 'Copied to clipboard!';
         const Toastclose = 'tempclose'
-        const result = Toast({Toastmsg,Toasttype,Toastclose});
+        const result = Toast(Toastmsg,Toasttype,Toastclose);
         expect(result).stringContaining("Copied to clipboard!");
 })
